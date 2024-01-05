9391d8d969d76ec06f7c05f7539a48d829d55be1
Andrew <andrew.weymes@outlook.com>
2024-01-05 18:10:59 +0800

    add preferred tech component

---
+++ frontend/src/components/preferredTech.svelte
@@ -0,0 +1,53 @@
+<div class="flex flex-wrap justify-center gap-4">
+	<h3 class="text-sit-orange">Preferred Technologies</h3>
+	<div class="flex flex-wrap justify-center gap-3 sm:gap-10">
+		<a href="https://svelte.dev/" class="flex flex-col items-center">
+			<div class="flex items-center grow">
+				<img src="/logos/svelte.svg" alt="Svelte logo" height="50" width="50" />
+			</div>
+			<p>Svelte</p>
+		</a>
+
+		<a href="https://nextjs.org/" class="flex flex-col items-center">
+			<div class="flex items-center grow">
+				<img src="/logos/nextjs.svg" alt="Nextjs logo" height="50" width="50" />
+			</div>
+			<p>Next.js</p>
+		</a>
+
+		<a href="https://react.dev/" class="flex flex-col items-center">
+			<div class="flex items-center grow">
+				<img src="/logos/react.svg" alt="React logo" height="50" width="50" />
+			</div>
+			<p>React.js</p>
+		</a>
+
+		<a href="https://htmx.org/" class="flex flex-col items-center">
+			<div class="flex items-center grow">
+				<img src="/logos/htmx.svg" alt="Htmx logo" height="50" width="50" />
+			</div>
+			<p>htmx</p>
+		</a>
+
+		<a href="https://go.dev/" class="flex flex-col items-center">
+			<div class="flex items-center grow">
+				<img src="/logos/go.svg" alt="Go logo" height="50" width="50" />
+			</div>
+			<p>Go</p>
+		</a>
+
+		<a href="https://www.typescriptlang.org/" class="flex flex-col items-center">
+			<div class="flex items-center grow">
+				<img src="/logos/typescript.svg" alt="Typescript logo" height="50" width="50" />
+			</div>
+			<p>Typescript</p>
+		</a>
+
+		<a href="https://openai.com/" class="flex flex-col items-center">
+			<div class="flex items-center grow">
+				<img src="/logos/openai.svg" alt="OpenAI logo" height="50" width="50" />
+			</div>
+			<p>OpenAI</p>
+		</a>
+	</div>
+</div>
