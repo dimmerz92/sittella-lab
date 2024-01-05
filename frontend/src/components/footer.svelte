59750f0177371e358ca3b0e45ca558c62c6c8fcc
Andrew <andrew.weymes@outlook.com>
2024-01-06 00:14:12 +0800

    add footer

---
+++ frontend/src/components/footer.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Contact from './contact.svelte';
+</script>
+
+<div class="flex flex-col gap-2 items-center bg-sit-lblue">
+	<Contact />
+	<div class="text-sm">Sittella Lab &#169; 2024. All rights reserved.</div>
+</div>
