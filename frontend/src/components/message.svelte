e6dd4db3f1302403f0596e5bfbf1bee52e395683
Andrew <andrew.weymes@outlook.com>
2024-01-08 23:51:03 +0800

    better styles

--- frontend/src/components/message.svelte
+++ frontend/src/components/message.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div
-	class={`w-fit p-4 rounded-md ${
+	class={`w-fit max-w-[70%] p-4 rounded-md text-xs ${
 		role === 'user' ? 'bg-sit-lblue ml-auto text-white' : 'bg-slate-200'
 	}`}
 >
