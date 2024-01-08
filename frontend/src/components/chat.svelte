c033556a274096171153a64f7204a829884d1ffa
Andrew <andrew.weymes@outlook.com>
2024-01-08 23:50:36 +0800

    improve logic and rendering

--- frontend/src/components/chat.svelte
+++ frontend/src/components/chat.svelte
@@ -4,6 +4,7 @@
 	import { slide } from 'svelte/transition';
 	import Message from './message.svelte';
 	import Loading from './loading.svelte';
+	import { onMount } from 'svelte';
 
 	let messages: GPTMessage[] = [];
 	let textArea: HTMLTextAreaElement;
@@ -12,6 +13,10 @@
 	let isLoading = false;
 	let lastEnter: Date | undefined;
 	const minTime = 2000; // milliseconds
+
+	onMount(() => {
+		textArea = document.getElementById('content') as HTMLTextAreaElement;
+	});
 
 	const toggleChat = async () => {
 		isVisible = !isVisible;
@@ -31,11 +36,9 @@
 	};
 
 	const alterEnter = (e: KeyboardEvent) => {
-		if (!initialised) textArea = document.getElementById('content') as HTMLTextAreaElement;
-
 		if (e.key === 'Enter' && !e.shiftKey) {
 			e.preventDefault();
-			if (textArea.value.length < 3) return;
+			if (textArea && textArea.value.length < 3) return;
 
 			//prevent spamming
 			if (!lastEnter || (lastEnter && new Date().getTime() - lastEnter.getTime() >= minTime)) {
@@ -68,41 +71,44 @@
 </script>
 
 <div class="fixed bottom-0 right-0 max-h-full max-w-full flex flex-col justify-end items-end z-20">
-	{#if isVisible}
+	<div
+		transition:slide={{ duration: 300, easing: linear }}
+		class={`${
+			isVisible ? 'h-screen sm:h-[768px] p-2' : 'h-0'
+		} flex flex-col bg-sit-yellow w-screen sm:w-96 gap-2 sm:mr-4 rounded-md shadow-lg overflow-hidden`}
+	>
+		<div class="relative flex justify-center items-center h-10">Virtual Assistant</div>
 		<div
-			transition:slide={{ duration: 300, easing: linear }}
-			class="flex flex-col bg-sit-yellow h-screen sm:h-[768px] w-screen sm:w-96 gap-2 p-2 sm:mr-4 rounded-md shadow-lg"
+			id="chat_window"
+			class="relative flex flex-col grow bg-white rounded-md gap-2 p-2 overflow-scroll"
 		>
-			<div class="relative flex justify-center items-center h-10">Virtual Assistant</div>
-			<div id="chat_window" class="relative flex flex-col grow bg-white rounded-md gap-2 p-2">
-				{#each messages as message}
-					<Message role={message.role} content={message.content} />
-				{/each}
-				{#if isLoading}
-					<div class="self-center mt-auto">
-						<Loading />
-					</div>
-				{/if}
-			</div>
-			<div id="chat" class="flex gap-2">
-				<textarea
-					name="content"
-					id="content"
-					on:keypress={alterEnter}
-					class="grow resize-none rounded-md"
-					rows="3"
-					required
-				></textarea>
-				<button
-					id="chat_button"
-					on:click={sendMessages}
-					class="bg-sit-dblue text-white aspect-square rounded-md"
-				>
-					Send
-				</button>
-			</div>
+			{#each messages as message}
+				<Message role={message.role} content={message.content} />
+			{/each}
+			{#if isLoading}
+				<div class="self-center mt-auto">
+					<Loading />
+				</div>
+			{/if}
 		</div>
-	{/if}
+		<div id="chat" class="flex gap-2">
+			<textarea
+				name="content"
+				id="content"
+				on:keypress={alterEnter}
+				class="grow resize-none rounded-md"
+				rows="3"
+				required
+			></textarea>
+			<button
+				id="chat_button"
+				on:click={sendMessages}
+				class="bg-sit-dblue text-white aspect-square rounded-md"
+			>
+				Send
+			</button>
+		</div>
+	</div>
 
 	<button type="submit" on:click={toggleChat} class="bg-sit-dblue p-1 m-2 rounded-full">
 		<img src="/logos/message.svg" alt="message icon" class="h-16" />
