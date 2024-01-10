1378913018745712e746cf0fa7b643c7bae15f97
Andrew <andrew.weymes@outlook.com>
2024-01-10 17:24:59 +0800

    proxy backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		proxy: {
-			'/api': 'http://localhost:8000'
+export default ({ mode }: { mode: string }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+	return defineConfig({
+		plugins: [sveltekit()],
+		server: {
+			proxy: {
+				'/api': process.env.VITE_API_PROXY
+			}
 		}
-	}
-});
+	});
+};
