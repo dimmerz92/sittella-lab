3020301935fce901dc09cef9a00a69d544c2e1f1
Andrew <andrew.weymes@outlook.com>
2024-01-10 17:04:19 +0800

    add spinner

---
+++ frontend/src/components/spinner.svelte
@@ -0,0 +1,21 @@
+<div class="spinner"></div>
+
+<style>
+	.spinner {
+		border: 5px solid theme(colors.sit-dblue);
+		border-top: 5px solid theme(colors.sit-orange);
+		border-radius: 50%;
+		width: 25px;
+		height: 25px;
+		animation: spin 2s linear infinite;
+	}
+
+	@keyframes spin {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+</style>
