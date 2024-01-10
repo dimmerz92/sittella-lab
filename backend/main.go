f40206fe0320f83cfb8f5e12b1ceae1992e7b051
Andrew <andrew.weymes@outlook.com>
2024-01-10 20:12:17 +0800

    remove Dockerfile and update root items

--- backend/main.go
+++ backend/main.go
@@ -4,17 +4,24 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/joho/godotenv"
 	"sittellalab.com.au/internal/api"
+	"sittellalab.com.au/internal/middleware"
 )
 
 func main() {
+	godotenv.Load()
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8000"
 	}
 
-	http.HandleFunc("/api/contact", api.EnquiryHandler)
-	http.HandleFunc("/api/chat", api.GPTChatHandler)
+	mux := http.NewServeMux()
 
-	http.ListenAndServe(":"+port, nil)
+	mux.HandleFunc("/api/contact", api.EnquiryHandler)
+	mux.HandleFunc("/api/chat", api.GPTChatHandler)
+
+	server := middleware.NewValidator(mux)
+
+	http.ListenAndServe(":"+port, server)
 }
