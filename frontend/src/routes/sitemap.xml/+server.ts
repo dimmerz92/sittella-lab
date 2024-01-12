0b30c7a575712855f4b1257bcd050d4c9558a4a4
Andrew <andrew.weymes@outlook.com>
2024-01-12 17:57:47 +0800

    add sitemap endpoint

---
+++ frontend/src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,18 @@
+export async function GET() {
+	return new Response(
+		`
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xlmns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://www.sittellalab.com.au/</loc>
+      <lastmod>2024-01-12</lastmod>
+    </url>
+    </urlset>
+    `.trim(),
+		{
+			headers: {
+				'Content-Type': 'application/xml'
+			}
+		}
+	);
+}
