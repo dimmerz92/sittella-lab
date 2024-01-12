51cca3046148aeacc78eb5525d39c570fa358d10
Andrew <andrew.weymes@outlook.com>
2024-01-12 18:07:37 +0800

    fix sitemap spelling

--- frontend/src/routes/sitemap.xml/+server.ts
+++ frontend/src/routes/sitemap.xml/+server.ts
@@ -2,11 +2,11 @@ export async function GET() {
 	return new Response(
 		`
     <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xlmns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-      <loc>https://www.sittellalab.com.au/</loc>
-      <lastmod>2024-01-12</lastmod>
-    </url>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>https://www.sittellalab.com.au/</loc>
+        <lastmod>2024-01-12</lastmod>
+      </url>
     </urlset>
     `.trim(),
 		{
